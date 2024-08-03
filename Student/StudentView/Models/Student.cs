c07483e71efc1fb6134b356578bdd2ead59f9316
Pati <patrickmistlberger@gmail.com>
2024-08-04 00:03:30 +0200

    update

---
+++ Student/StudentView/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿namespace StudentView.Models;
+
+public class Student
+{
+    public int Id { get; set; } = default;
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public DateTime Birthday { get; set; }
+    public int Classroom { get; set; }
+
+    public Student() {}
+
+    public Student(int id, string firstName, string lastName, DateTime birthday, int classroom)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        Birthday = birthday;
+        Classroom = classroom;
+    }
+}
